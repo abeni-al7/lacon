95ea71466a9b815d8f9fbe3c229f256df1b65384
abeni-al7 <abenezeralebachew3@gmail.com>
2026-07-12 20:49:05 +0300

    enable analytics

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { useState } from 'react'
 import Header from './components/Header'
 import ModeSelector from './components/ModeSelector'
